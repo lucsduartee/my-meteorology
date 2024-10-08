15dc00f346226ce53fd1b8460ea4126ad0ed879d
Lucas Duarte <lucs.rkt@gmail.com>
2024-10-08 20:54:00 -0300

    feat: implement cities select

--- src/components/CitiesSelect/CitiesSelect.tsx
+++ src/components/CitiesSelect/CitiesSelect.tsx
@@ -1,11 +1,30 @@
+import { useState, useContext, FormEvent, useEffect } from "react";
+import { CityContext, ICity } from "@/contexts/CityProvider";
 import styles from './CitiesSelect.module.css'
 
 export default function CitiesSelect() {
+  const cityContext = useContext(CityContext);
+  const [citySelected, setCitySelected] = useState(cityContext?.currentCity?.city.name);
+  
+  useEffect(() => {
+    setCitySelected(cityContext?.currentCity?.city.name)
+  }, [cityContext?.currentCity?.city.name]);
+
+  function handleSelectOnChange(event: FormEvent<HTMLSelectElement>) {
+    event.preventDefault();
+
+    cityContext?.setCurrentCity(cityContext?.cities.find((city: ICity) => city.city.name === citySelected));
+  }
+
   return (
     <div className={styles.citiesSelectContainer}>
       <label htmlFor="lang">Cidades pesquisadas recentemente</label>
-      <select name="cities" id="cities">
-        <option value="javascript">Belford Roxo, RJ</option>
+      <select name="cities" id="cities" value={citySelected} onChange={handleSelectOnChange}>
+        {
+          cityContext?.cities.length !== 0 && cityContext?.cities.map((city: ICity, index) => (
+            <option key={index} value={city.city.name}>{city.city.name}, {city.city.state}</option>
+          ))
+        }
       </select>
     </div>
   );
