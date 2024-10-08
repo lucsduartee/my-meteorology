3b556c1ce3f283c98ee811873012042d2417bd4b
Lucas Duarte <lucs.rkt@gmail.com>
2024-10-08 15:46:39 -0300

    feat: try call hg api

--- src/components/CitySearch/CitySearch.tsx
+++ src/components/CitySearch/CitySearch.tsx
@@ -1,23 +1,25 @@
-import { FormEvent, useState } from "react";
-// import { CityContext } from '@/contexts/CityProvider';
+import { FormEvent, useState, useContext } from "react";
+import { CityContext } from '@/contexts/CityProvider';
 
-// const OPEN_WEATHER_BASE_URL = "http://api.openweathermap.org/geo/1.0/direct";
-// const OPEN_WEATHER_API_KEY = "ec86d72c6e35f76377e32fda62b0247f";
-// const HG_BRASIL_BASE_URL = "https://api.hgbrasil.com/weather";
-// const HG_BRASIL_API_KEY = 
-// https://api.hgbrasil.com/weather?key=SUA-CHAVE&lat=-23.682&lon=-46.875&user_ip=remote
+const OPEN_WEATHER_BASE_URL = "http://api.openweathermap.org/geo/1.0/direct";
+const OPEN_WEATHER_API_KEY = "ec86d72c6e35f76377e32fda62b0247f";
+const HG_BRASIL_BASE_URL = "https://api.hgbrasil.com/weather";
+const HG_BRASIL_API_KEY = "505c8bec" 
+// https://api.hgbrasil.com/weather?key=SUA-CHAVE&
 export default function CitySearch() {
   const [cityName, setCityName] = useState('');
-  // const cityContext = useContext(CityContext);
+  const cityContext = useContext(CityContext);
+  console.log('cityContext', cityContext);
 
   async function onSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
-    // const openWeatherResponse = await fetch(`${OPEN_WEATHER_BASE_URL}?q=${cityName}&appid=${OPEN_WEATHER_API_KEY}`);
-    // const [city]  = await openWeatherResponse.json();
-    // const { lat, lon } = city;
+    const openWeatherResponse = await fetch(`${OPEN_WEATHER_BASE_URL}?q=${cityName}&appid=${OPEN_WEATHER_API_KEY}`);
+    const [city]  = await openWeatherResponse.json();
+    const { lat, lon } = city;
 
-    // const res = await fetch(`${BASE_URL}?q=${cityName}&appid=${OPEN_WEATHER_API_KEY}`); 
+    const hgBrasilResponse = await fetch(`${HG_BRASIL_BASE_URL}?key=${HG_BRASIL_API_KEY}&lat=${lat}&lon=${lon}`);
+    console.log('hgBrasilResponse', hgBrasilResponse);
   }
   return (<form onSubmit={onSubmit}>
     <label htmlFor="city">Consulte a previsão do tempo em uma cidade</label>
